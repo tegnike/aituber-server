523e73b594b400347e16818d129330b47ddeae21
tegnike <o3nike.teg.14@gmail.com>
2024-06-04 22:48:16 +0200

    send_messageエンドポイント追加&調整

--- app/services/websocket_service.py
+++ app/services/websocket_service.py
@@ -1,5 +1,5 @@
 import json
-import base64
+
 
 async def send_websocket_message(websocket, message, type):
     # type別にrole変数に値を設定
@@ -8,7 +8,7 @@ async def send_websocket_message(websocket, message, type):
     if websocket and message != "":
         json_data = json.dumps({"role": role, "text": message}, ensure_ascii=False)
         print(f"Sending message: {json_data}")
-        await websocket.send({"type": "websocket.send", "text": json_data})
-        print(f"Send cmplete.")
+        await websocket.send_text(json_data)
+        print("Send cmplete.")
     else:
         print("Can't send message, WebSocket connection is closed.")
