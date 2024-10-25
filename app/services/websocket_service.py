8f09eca06ae5458f3646908cd22b4a824577ef98
tegnike <o3nike.teg.14@gmail.com>
2024-10-25 19:41:57 +0200

    websocketの送信プロパティ更新

--- app/services/websocket_service.py
+++ app/services/websocket_service.py
@@ -2,19 +2,19 @@ import json
 import asyncio
 
 
-async def send_websocket_message(websocket, message, type, state=""):
-    # type別にrole変数に値を設定
-    role = "assistant" if type == "message" else type
+async def send_websocket_message(websocket, message, role, type="", emotion="neutral"):
+    role = "assistant" if role == "message" else role
 
     if not websocket:
         print("Can't send message, WebSocket connection is closed.")
         return
-    elif state == "" and message == "":
+    elif type == "" and message == "":
         print("Can't send message, message is empty.")
         return
     else:
         json_data = json.dumps(
-            {"role": role, "text": message, "state": state}, ensure_ascii=False
+            {"role": role, "text": message, "emotion": emotion, "type": type},
+            ensure_ascii=False,
         )
         print(f"Sending message: {json_data}")
         await websocket.send_text(json_data)
